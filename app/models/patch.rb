1e6de72317d6c4f41c0924cbc5687374c9de3afb
Amanda McGuire <aruwart001@gmail.com>
2021-08-06 09:51:08 -0600

    Add find patch by plant id and plot id method

--- app/models/patch.rb
+++ app/models/patch.rb
@@ -1,4 +1,8 @@
 class Patch < ApplicationRecord
   belongs_to :plant
   belongs_to :plot
+
+  def self.find_plant_plot(plot_id, plant_id)
+    where(plot_id: plot_id, plant_id: plant_id).first
+  end
 end
