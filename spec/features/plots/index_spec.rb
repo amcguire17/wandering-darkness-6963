67dab6b137a492ead520026269afe358c5c4ab62
Amanda McGuire <aruwart001@gmail.com>
2021-08-06 09:50:50 -0600

    Add remove plant from plot specs

--- spec/features/plots/index_spec.rb
+++ spec/features/plots/index_spec.rb
@@ -24,24 +24,50 @@ RSpec.describe 'Plots Index Page' do
 
     visit "/plots"
   end
-  it 'lists all plots' do
-    expect(page).to have_content(@plot_1.number)
-    expect(page).to have_content(@plot_2.number)
-    expect(page).to have_content(@plot_3.number)
+  describe 'plot lists' do
+    it 'lists all plots' do
+      expect(page).to have_content(@plot_1.number)
+      expect(page).to have_content(@plot_2.number)
+      expect(page).to have_content(@plot_3.number)
+    end
+    it 'lists all plants in each plot' do
+      within("#plot-#{@plot_1.id}") do
+        expect(page).to have_content(@plant_1.name)
+        expect(page).to have_content(@plant_2.name)
+        expect(page).to_not have_content(@plant_3.name)
+      end
+      within("#plot-#{@plot_2.id}") do
+        expect(page).to have_content(@plant_3.name)
+        expect(page).to have_content(@plant_4.name)
+        expect(page).to have_content(@plant_7.name)
+        expect(page).to_not have_content(@plant_5.name)
+      end
+      within("#plot-#{@plot_3.id}") do
+        expect(page).to have_content(@plant_5.name)
+        expect(page).to have_content(@plant_6.name)
+        expect(page).to_not have_content(@plant_1.name)
+      end
+    end
   end
-  it 'lists all plants in each plot' do
-    within("#plot-#{@plot_1.id}") do
-      expect(page).to have_content(@plant_1.name)
-      expect(page).to have_content(@plant_2.name)
+  describe 'remove plants from plot' do
+    it 'has a link to remove plants from plot' do
+      within("#plant-#{@plant_1.id}") do
+        expect(page).to have_link('Remove')
+      end
+      within("#plant-#{@plant_3.id}") do
+        expect(page).to have_link('Remove')
+      end
+      within("#plant-#{@plant_5.id}") do
+        expect(page).to have_link('Remove')
+      end
     end
-    within("#plot-#{@plot_2.id}") do
-      expect(page).to have_content(@plant_3.name)
-      expect(page).to have_content(@plant_4.name)
-      expect(page).to have_content(@plant_7.name)
-    end
-    within("#plot-#{@plot_3.id}") do
-      expect(page).to have_content(@plant_5.name)
-      expect(page).to have_content(@plant_6.name)
+    it 'removes plant from plot when link is clicked' do
+      within("#plant-#{@plant_7.id}") do
+        click_link 'Remove'
+        expect(current_path).to eq("/plots")
+      end
+      expect(page).to_not have_css("#plant-#{@plant_7.id}")
+      expect(@plant_7).to be_a(Plant)
     end
   end
 end
--- spec/models/patch_spec.rb
+++ spec/models/patch_spec.rb
@@ -5,4 +5,18 @@ RSpec.describe Patch do
     it { should belong_to(:plant) }
     it { should belong_to(:plot) }
   end
+
+  describe 'class methods' do
+    describe '.find_plant_plot' do
+      it 'can find the patch by plant and plot ids' do
+        garden_1 = Garden.create!(name: 'Sunset Gardens', organic: false)
+        plot_1 = garden_1.plots.create!(number: 15, size: 'Medium', direction: 'West')
+        plant_1 = Plant.create!(name: 'carrot', description: 'keep watered and fertilized', days_to_harvest: 80)
+        plant_2 = Plant.create!(name: 'strawberries', description: 'plant in matted rows', days_to_harvest: 100)
+        patch_1 = Patch.create!(plot_id: plot_1.id, plant_id: plant_1.id)
+        patch_2 = Patch.create!(plot_id: plot_1.id, plant_id: plant_2.id)
+        expect(Patch.find_plant_plot(plot_1.id, plant_1.id)).to eq(patch_1)
+      end
+    end
+  end
 end
